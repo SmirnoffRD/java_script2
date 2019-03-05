607d6641e502bef82b636b72099a8b57485a7764
SmirnoffRD <38382018+SmirnoffRD@users.noreply.github.com>
2019-03-05 15:54:39 +0300

    DZ4 done index4.html

--- js/main.js
+++ js/main.js
@@ -2,35 +2,6 @@
 /* jshint -W097 */
 /*jslint devel: true */
 
-// const productslist = [
-//     {id: 1, title: 'Notebook', price: 2000},
-//     {id: 2, title: 'Mouse', price: 20},
-//     {id: 3, title: 'Keyboard', price: 35},
-//     {id: 4, title: 'Gamepad', price: 58},
-//     {id: 5, title: 'Chair', price: 200},
-// ];
-
-
-
-// const renderPage = (list = [{},]) => {
-//     const productsList = list.map(item => renderProduct(item.id, item.title, item.price));
-//     document.querySelector('.products').innerHTML = productsList.join('');
-// };
-
-// renderProduct = (id = 0, title = 'sample', price = 0) => {
-//     return `<div class="product-item">
-//                 <h3>${title}</h3>
-//                 <p>${price}</p>
-//                 <button 
-//                 data-id="${id}"
-//                 data-price="${price}"
-//                 data-title="${title}"
-//                 >Купить</button>
-//             </div>`;
-// };
-
-// renderPage();
-
 const products = {
     /**
      * list of products
